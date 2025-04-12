19cc982a9082f6bb9f84dde6ead8c3dfdc3715e1
laineytrann <laineyt@uci.edu>
2025-04-12 10:43:42 -0700

    Update bithack.ino

--- bithack/bithack.ino
+++ bithack/bithack.ino
@@ -1,15 +1,25 @@
+#include <HardwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
 const int shortButton = 7;
 const int longButton;
 const int startEnd;
 const int LEDSB = 15;
 const int LEDLB;
 const int LEDSE;
+const int rxPin = ;
+const int txPin = ;
+
+HardwareSerial mySerial1(1);
+DFRobotDFPlayerMini player;
 
 //turns on the green led for 1s when the short Button is pressed
 void short_light() {
   digitalWrite(LEDSB, HIGH);
+  tone(3, 783, 500);
   delay(1000);
   digitalWrite(LEDSB, LOW);
+  
 }
 
 void long_light() {
@@ -21,6 +31,12 @@ void long_light() {
 void setup() {
   pinMode(shortButton, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(shortButton), short_light, FALLING);
+  pinMode(longButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(longButton), long_light, FALLING);
+
+  player.volume(20);
+  player.play(1);
+  mySerial1.begin(9600, SERIAL_8N1, rxPin, txPin);
 
 }
 
