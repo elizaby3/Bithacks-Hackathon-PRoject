b9ebaf56baa19ce76a85da660792e12eaf32a507
HC-Chloe <huyenchn@uci.edu>
2025-04-12 15:44:49 -0700

    Update bithack.ino

--- bithack/bithack.ino
+++ bithack/bithack.ino
@@ -1,128 +1,32 @@
+#include <Timer.h>
 #include <HardwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-#include <timer.h>
->>>>>>> Stashed changes
+#include <LiquidCrystal_I2C.h>
+
+const int alphaSize = 26;
 
 const int shortButton = 7;
-const int longButton;
-const int startEnd;
-const int LEDSB = 15;
-const int LEDLB;
-const int LEDSE;
-const int rxPin = ;
-const int txPin = ;
+const int longButton = 6;
+const int stopButton = 15;
 
-int startCountDown;
-HardwareSerial mySerial1(1);
-DFRobotDFPlayerMini player;
+const int LEDSB = 5;
+const int LEDLB = 4;
+const int LEDSE = 16;
 
-<<<<<<< Updated upstream
-=======
-=======
+int startCountDown = 0;
+int count = 0;
+int randomIndex;
 
-const int shortButton = 7;
-const int longButton;
-const int startEnd;
-const int LEDSB = 15;
-const int LEDLB;
-const int LEDSE;
-const int rxPin = ;
-const int txPin = ;
+volatile bool lb_pressed = false;
+volatile bool sb_pressed = false;
+volatile bool stop_pressed = false;
 
-int startCountDown;
-HardwareSerial mySerial1(1);
-DFRobotDFPlayerMini player;
->>>>>>> 3462fd8bbf78e563b352084ba248ee989133445b
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//turns on the green led for 1s when the short Button is pressed
-void short_light() {
-  digitalWrite(LEDSB, HIGH);
-  tone(3, 783, 1000);
-  startCountDown = 1;
-  digitalWrite(LEDSB, LOW);
-  
-}
-
-<<<<<<< HEAD
-  if (count%2 == 0) {
-    digitalWrite(LEDSB, LOW);
-  } else {
-    digitalWrite(LEDSB, HIGH);
-  }
-
->>>>>>> Stashed changes
-//turns on the green led for 1s when the short Button is pressed
-void short_light() {
-  digitalWrite(LEDSB, HIGH);
-  tone(3, 783, 1000);
-  startCountDown = 1;
-  digitalWrite(LEDSB, LOW);
-  
-}
-
-=======
->>>>>>> 3462fd8bbf78e563b352084ba248ee989133445b
-void long_light() {
-  digitalWrite(LEDLB, HIGH);
-  tone(3, 783, 3000);
-
-  startCountDown = 1;
-<<<<<<< Updated upstream
-  digitalWrite(LEDLB, LOW);
-=======
-<<<<<<< HEAD
-  longLightCount = 1;
-=======
-  digitalWrite(LEDLB, LOW);
->>>>>>> 3462fd8bbf78e563b352084ba248ee989133445b
->>>>>>> Stashed changes
-}
-
-void setup() {
-  pinMode(shortButton, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(shortButton), short_light, FALLING);
-  pinMode(longButton, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(longButton), long_light, FALLING);
-
-  player.volume(20);
-  player.play(1);
-  mySerial1.begin(9600, SERIAL_8N1, rxPin, txPin);
-
-}
-
-int longLightCount = 3000;
-
-void loop() {
-
-  if(startCountDown && longLightCount > 0) {
-    longLightCount--;
-    digitalWrite(LEDLB, HIGH);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 3462fd8bbf78e563b352084ba248ee989133445b
->>>>>>> Stashed changes
-  }else{
-    digitalWrite(LEDLB, LOW);
-    startCountDown = 0;
-  }
-}
-<<<<<<< Updated upstream
-=======
-
-
-struct Alphabet{ //creates the "dictionary" that allows to access the features of each letter in the alphabet
-  String letter;  //accesses the letter
-  String morseCode; //accesses the morse code that corresponds to each letter 
-  //the morseCode is integers as a string because we'll use the compare function
-}
-
-
+struct Alphabet{
+  String letter;  
+  String morseCode; 
+};
 Alphabet alphaProp[] = { //creates an array that includes all letters of the alphabet and its properties as properties 
   {"A", "01"},
   {"B", "1000"},
@@ -150,8 +54,100 @@ Alphabet alphaProp[] = { //creates an array that includes all letters of the alp
   {"X", "1001"},
   {"Y", "1011"},
   {"Z", "1100"},
+};
+String buttonLog = "";
 
+void short_light() {
+  sb_pressed = true;
+}
+
+void long_light(){
+  lb_pressed = true;
+}
+
+void stop() {
+  stop_pressed = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+  pinMode(shortButton, INPUT_PULLUP);
+  pinMode(LEDSB, OUTPUT);
+  
+  pinMode(longButton, INPUT_PULLUP);
+  pinMode(LEDLB, OUTPUT);
+
+  pinMode(stopButton, INPUT_PULLUP);
+  pinMode(LEDSE,OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(shortButton), short_light, FALLING);
+  attachInterrupt(digitalPinToInterrupt(longButton), long_light, FALLING);      //setup for pinmods and interrupts
+  attachInterrupt(digitalPinToInterrupt(stopButton), stop, FALLING);
+
+  lcd.init();
+  lcd.backlight();
+
+  pickRandomLetter();
 }
 
 
->>>>>>> Stashed changes
+
+void pickRandomLetter() {
+  randomIndex = random(0, alphaSize); // Get random index from 0 to alphaSize - 
+
+  Alphabet currentLetter = alphaProp[randomIndex];
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Morse for letter: ");
+  lcd.setCursor(1,0);
+  lcd.print(currentLetter.letter);
+  delay(10000);
+}
+
+
+void morseCheck() {
+  bool found = false;
+    if (buttonLog == alphaProp[randomIndex].morseCode) {
+      Serial.println(alphaProp[randomIndex].letter);
+      found = true;
+    }
+  if (!found) {
+    Serial.println("No match found.");
+  }
+
+  pickRandomLetter();
+
+}
+
+void loop() {
+
+  if (sb_pressed) {
+    digitalWrite(LEDSB, HIGH);
+    buttonLog += "0"; 
+    Serial.println("Short button pressed. Log: " + buttonLog);
+    delay(1000);
+    digitalWrite(LEDSB, LOW);
+    sb_pressed = false;
+  }
+
+  if (lb_pressed) {
+    digitalWrite(LEDLB, HIGH);
+    buttonLog += "1";
+    Serial.println("Long button pressed. Log: " + buttonLog);
+    delay(2000);
+    digitalWrite(LEDLB, LOW);
+    lb_pressed = false;
+  }
+
+  if (stop_pressed) {
+    morseCheck();
+    digitalWrite(LEDSE, HIGH);
+    delay(1000);
+    digitalWrite(LEDSE, LOW);
+    stop_pressed = false;
+  }
+}
