059d65f53c48ea99def6edefb7102d1b57deba9f
Elizabeth Yang <elizaby3@uci.edu>
2025-04-12 09:20:35 -0700

    Added code for short led

--- bithack/bithack.ino
+++ bithack/bithack.ino
@@ -1,13 +1,20 @@
 const int shortButton = 7;
 const int longButton;
 const int startEnd;
-const int LEDSB;
+const int LEDSB = 15;
 const int LEDLB;
 const int LEDSE;
 
+//turns on the green led for 1s when the short Button is pressed
+void short_light() {
+  digitalWrite(LEDSB, HIGH);
+  delay(1000);
+  digitalWrite(LEDSB, LOW);
+}
 
 void setup() {
-  
+  pinMode(shortButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(shortButton), short_light, FALLING);
 
 }
 
