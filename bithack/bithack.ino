29d044144be5b0200719c04873d0605b4b57a05d
laineytrann <laineyt@uci.edu>
2025-04-12 15:32:58 -0700

    Update bithack.ino

--- bithack/bithack.ino
+++ bithack/bithack.ino
@@ -3,116 +3,33 @@
 #include <Timer.h>
 Timer timer;
 
+const int alphaSize = 26;
+
 const int shortButton = 7;
 const int longButton = 6;
-//const int startEnd;
+const int stopButton = 15;
+
 const int LEDSB = 5;
 const int LEDLB = 4;
-//const int LEDSE;
+const int LEDSE = 16;
+const int buzzer = 17; ///
+
 int startCountDown = 0;
 int count = 0;
 int randomIndex;
 
-const int txPin = 43;
-const int rxPin = 44;
+const int txPin = 43; //
+const int rxPin = 44; //
 
 volatile bool lb_pressed = false;
 volatile bool sb_pressed = false;
-
-
-void short_light() {
-  sb_pressed = true;
-}
-
-void long_light(){
-  lb_pressed = true;
-}
-
-void long_light(){
-  lb_pressed = true; 
-}
-
-void setup() {
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(10);
-  }
-  pinMode(shortButton, INPUT_PULLUP);
-  pinMode(LEDSB, OUTPUT);
-  pinMode(longButton, INPUT_PULLUP);
-  pinMode(LEDLB, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(shortButton), short_light, FALLING);
-  attachInterrupt(digitalPinToInterrupt(longButton), long_light, FALLING);      //setup for pinmods and interrupts
-
-}
-
-String buttonLog = "";
-
-void loop() {
-
-  if (sb_pressed) {
-    digitalWrite(LEDSB, HIGH);
-    buttonLog += "0"; 
-    Serial.println("Short button pressed. Log: " + buttonLog);
-    delay(1000);
-    digitalWrite(LEDSB, LOW);
-    sb_pressed = false;
-  }
-
-  if (lb_pressed) {
-    digitalWrite(LEDLB, HIGH);
-    buttonLog += "1";
-    Serial.println("Long button pressed. Log: " + buttonLog);
-    delay(2000);
-    digitalWrite(LEDLB, LOW);
-    lb_pressed = false;
-  }
-
-  if (stop_pressed) {
-    morseCheck();
-  }
-
-  if (lb_pressed) {
-    digitalWrite(LEDLB, HIGH);
-    buttonLog += "1";
-    Serial.println("Long button pressed. Log: " + buttonLog);
-    delay(2000);
-    digitalWrite(LEDLB, LOW);
-    lb_pressed = false;
-  }
-
-}
+volatile bool stop_pressed = false;
 
 struct Alphabet{
   String letter;  
   String morseCode; 
 };
-
-void pickRandomLetter() {
-  randomIndex = random(0, alphaSize); // Get random index from 0 to alphaSize - 1
-  currentLetter = alphaProp[randomIndex];
-
-  Serial.print("New round! Morse for letter: ");
-  Serial.println(currentLetter.letter);
-  Serial.print("Expected Morse code: ");
-  Serial.println(currentLetter.morseCode);
-}
-
-void morseCheck() {
-  bool found = false;
-    if (buttonLog == alphaProp[randomIndex].morseCode) {
-      Serial.println(alphaProp[randomIndex].letter);
-      found = true;
-      //play congrats
-      break;
-    }
-  if (!found) {
-    //play rickroll
-    Serial.println("Incorrect input");
-  }
-}
-
-Alphabet alphaProp[] = { //creates an array that includes all letters of the alphabet and its properties as properties 
+Alphabet alphaProp[] = {
   {"A", "01"},
   {"B", "1000"},
   {"C", "1010"},
@@ -139,6 +56,101 @@ Alphabet alphaProp[] = { //creates an array that includes all letters of the alp
   {"X", "1001"},
   {"Y", "1011"},
   {"Z", "1100"},
+};
+String buttonLog = "";
+
+void short_light() {
+  sb_pressed = true;
+  tone(16, 783, 1000); //
 }
 
+void long_light(){
+  lb_pressed = true;
+  tone(16, 783, 3000); //
+}
+
+void stop() {
+  stop_pressed = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+  delay(10);
+  }
+  mySerial1.begin(9600, SERIAL_8N1, rxPin, txPin);
+  player.volume(20); //
+
+  
+  pinMode(shortButton, INPUT_PULLUP);
+  pinMode(LEDSB, OUTPUT);
+
+  pinMode(longButton, INPUT_PULLUP);
+  pinMode(LEDLB, OUTPUT);
+
+  pinMode(stopButton, INPUT_PULLUP);
+  pinMode(LEDSE, OUTPUT);
+
+  pinMode(buzzer, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(shortButton), short_light, FALLING);
+  attachInterrupt(digitalPinToInterrupt(longButton), long_light, FALLING);    
+  attachInterrup(digitalPinToInterrupt(stopButton), stop, FALLING);
+}
+
+void pickRandomLetter() {
+  randomIndex = random(0, alphaSize);
+
+  Alphabet currentLetter = alphaProp[randomIndex];
+
+  Serial.print("New round! Morse for letter: ");
+  Serial.println(currentLetter.letter);
+  Serial.print("Expected Morse code: ");
+  Serial.println(currentLetter.morseCode);
+}
+
+void morseCheck() {
+  bool found = false;
+    if (buttonLog == alphaProp[randomIndex].morseCode) {
+      Serial.println(alphaProp[randomIndex].letter);
+      found = true;
+      player.play(1); //use diff value for diff track number
+      break;
+    }
+  if (!found) {
+    player.play(1); //use diff value for diff track number, whatever track corresponds to rick roll
+    Serial.println("Incorrect input");
+    delay(10000);
+    player.stop();
+  }
+}
+
+void loop() {
+
+  if (sb_pressed) {
+    digitalWrite(LEDSB, HIGH);
+    buttonLog += "0"; 
+    Serial.println("Short button pressed. Log: " + buttonLog);
+    delay(1000);
+    digitalWrite(LEDSB, LOW);
+    sb_pressed = false;
+  }
+
+  if (lb_pressed) {
+    digitalWrite(LEDLB, HIGH);
+    buttonLog += "1";
+    Serial.println("Long button pressed. Log: " + buttonLog);
+    delay(2000);
+    digitalWrite(LEDLB, LOW);
+    lb_pressed = false;
+  }
+
+  if (stop_pressed) {
+    morseCheck();
+    digitalWrite(LEDSE, HIGH);
+    delay(1000);
+    digitalWrite(LEDSE, LOW);
+    stop_pressed = false;
+  }
+}
 
